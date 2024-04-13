a6dd8c27ef934343fc90c616b7f5f5abb1021488
NaughtyKomodo <127680259+NaughtyKomodo@users.noreply.github.com>
2024-04-13 18:09:30 +0700

    Update func.py

--- func.py
+++ func.py
@@ -14,11 +14,11 @@ from datetime import datetime as dt
 
 tt = tweepy.Client(
     #Consumer Keys
-    consumer_key= os.environ['CONSUMER_KEY'],
-    consumer_secret= os.environ['CONSUMER_SECRET'],
+    consumer_key= os.environ['DImpHCTCUHbGIoKLZsbAg59LH'],
+    consumer_secret= os.environ['9ClQMOinOtpHIjE5WThlpoLMlElAWwYfQx2V27STZfXqz61fKt'],
     # Access Token and Secret
-    access_token= os.environ['ACCESS_TOKEN'],
-    access_token_secret= os.environ['ACCESS_TOKEN_SECRET'])
+    access_token= os.environ['1720059598336569344-ZBgVD8EmEBOcivPmMDHTiKklXSsvod'],
+    access_token_secret= os.environ['UTvadnzma2drbQ1KuAanqI3cM04krqUxd8xm0V2Lx6NYD'])
 
 # ## Defining Functions
 
